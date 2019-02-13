96190a1043a17068ed3dfe1fef0c09ffaba200db
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-13 00:26:45 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(sentence1)
+ new = []
+  new = sentence1.split
+  new.map {|word| word.reverse}.join(" ")
+
+end 
+
+def reverse_each_word(sentence2)
+ new = []
+  new = sentence2.split
+  new.collect {|word| word.reverse}.join(" ")
+
+end 
+
+
+
